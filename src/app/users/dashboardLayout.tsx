74fda6d7a9df1093582d320c63d25c9d51d25f71
m-atique <atiquenwl@gmail.com>
2023-08-11 23:22:41 +0500

    mobile view created

--- src/app/users/dashboardLayout.tsx
+++ src/app/users/dashboardLayout.tsx
@@ -23,12 +23,12 @@ export default function DashboardLayout({ children, isDashboardPage }: RootLayou
   return (
     <div className={`${inter.className} flex min-h-screen bg-background text-primary`}>
       <div className="flex flex-grow">
-        {isDashboardPage ? (
+        {/* {isDashboardPage ? (
           <Sidebar className="w-64" /> // Set the width of the sidebar as needed
-        ) : null}
-        <div className="flex-grow">
-          <div className="border-b">
-            <div className="flex h-16 items-center px-4">
+        ) : null} */}
+        <div className="flex-grow ">
+          <div className="border-b bg-red-500 p-2 hidden md:block">
+            <div className="flex h-16 items-center px-4 bg-orange-500">
               <TeamSwitcher />
               <MainNav className="mx-6" />
               <div className="ml-auto flex items-center space-x-4">
@@ -42,7 +42,27 @@ export default function DashboardLayout({ children, isDashboardPage }: RootLayou
               )}
             </div>
           </div>
-          <main className="p-8">{children}</main>
+          <div className= 'bg-violet-600'>
+            {/* ====user toggler and menu */}
+            <div className="flex flex-row justify-end gap-2 py-2 px-3">
+            <UserNav />
+            {settings.themeToggleEnabled && (
+                <div className="">
+                  <ModeToggle />
+                </div>
+              )}
+            </div>
+            <div className="flex justify-end">
+              <div className=" flex flex-col gap-2 w-2/4 bg-orange-300 divide-x-2 pb-4 pt-2">
+              <Search />
+              <TeamSwitcher />
+              <MainNav className="flex flex-col gap-2 justify-start" />
+              
+                </div>
+                </div>
+          </div>
+
+          {/* <main className="p-8">{children}</main> */}
         </div>
       </div>
     </div>
